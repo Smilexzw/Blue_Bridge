9824f3e65c7816e3263da3b9eda8b14efc70a865
SMILEXZW <1114998422@qq.com>
2019-03-01 16:50:22 +0800

    _2015_01三角形

---
+++ src/competition/_2015_01三角形.java
@@ -0,0 +1,15 @@
+package competition;
+
+/**
+ * @Auther: xuzhangwang
+ * @Description:
+ *
+三角形面积
+
+如【图1】所示。图中的所有小方格面积都是1。
+那么，图中的三角形面积应该是多少呢？
+
+请填写三角形的面积。不要填写任何多余内容或说明性文字。
+ */
+public class _2015_01三角形 {
+}
